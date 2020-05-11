71fd214c0519f11b1d5438198c92046828709967
Elkarloz <croameneses@gmail.com>
2020-05-11 15:22:09 -0500

    CRUD Provedor

--- src/views/Provedor.vue
+++ src/views/Provedor.vue
@@ -1,79 +1,129 @@
 <template>
   <div class="row col-md-12" id="card">
     <div class="col-md-8" style="padding-top: 5%">
-      <form class="form-inline my-2 my-lg-0 text-center" id="search-mobile"><!-- Btn input -->
-        <input class="form-control mr-sm-2 ml-auto mr-auto" type="search" placeholder="Search" aria-label="Search" id="formulario" @keyup="buscar()">
+      <form class="form-inline my-2 my-lg-0 text-center" id="search-mobile"><!-- Btn input consultar -->
+        <input class="form-control mr-sm-2 ml-auto mr-auto" type="search" placeholder="Search" aria-label="Search" id="formulario" @keyup="buscar()" autocomplete=off>
         <button class="btn btn-outline-success my-2 my-sm-0 ml-auto mr-auto" type="submit" id="btn" >Search</button>
       </form>
       <div style="padding-top: 5%;"></div><!-- Tabla -->
-      <div class="ml-auto mr-auto" id="table-mod" style="overflow-y: scroll">
+      <div class="ml-auto mr-auto" id="table-mod" style="overflow-y: scroll;">
         <table class="table table-hover">
           <thead class="thead-dark">
             <tr>
             <th class="text-center">Nit</th>
             <th class="text-center">Teléfono</th>
-            <th class="text-center">Nombre</th>
-            <th class="text-center">Apellido</th>
-            <th class="text-center">Sexo</th>
-            <th class="text-center">Estado</th>
-            <th class="text-center">Accion</th>
+            <th class="text-center">Email</th>
+            <th class="text-center">N Empresa</th>
+            <th class="text-center">Tipo Producto</th>
+            <th class="text-center">Cuidad</th>
+            <th class="text-center">Direccion</th>
+            <th></th>
             </tr>
           </thead>
           <tbody>
             <tr v-for="(item, index) in proveedores" :key="index">
               <th class="text-center">{{item.ProvNit}}</th>
-              <td class="text-center">{{item.ProvTelefono}}</td>
-              <td class="text-center">{{item.ProvEmail}}</td>
-              <td class="text-center">{{item.ProvNEmpresa}}</td>
-              <td class="text-center">{{item.ProvTipProducto}}</td>
-              <td class="text-center">{{item.ProvCiudad}}</td>
-              <td class="text-center">{{item.ProvDireccion}}</td>
+              <th class="text-center">{{item.ProvTelefono}}</th>
+              <th class="text-center">{{item.ProvEmail}}</th>
+              <th class="text-center">{{item.ProvNEmpresa}}</th>
+              <th class="text-center">{{item.ProvTipProducto}}</th>
+              <th class="text-center">{{item.ProvCiudad}}</th>
+              <th class="text-center">{{item.ProvDireccion}}</th>
+              <th>
+                <input type="radio" name="radio" @click="consultarProvID(item.ProvId)" data-toggle="modal" data-target="#exampleModal">
+              </th>
             </tr>
           </tbody>
         </table>
       </div>
     </div>
-    <div class="col-md-4 my-auto"> <!-- inputs -->
+    <div class="col-md-4 my-auto" v-if="this.editar === true">
+        <div class="input-group">
+          <span class="my-auto" style="padding-right: 5px;">Nit: editar</span>
+          <input type="text" aria-label="First name" class="form-control" v-model="proveedoresID.ProvNit">
+        </div>                       
+        <div class="input-group separacion">
+          <span class="my-auto" style="padding-right: 5px;">Teléfono:</span>
+          <input type="text" aria-label="First name" class="form-control" v-model="proveedoresID.ProvTelefono">
+        </div>                       
+        <div class="input-group separacion">
+          <span class="my-auto" style="padding-right: 5px;">Email:</span>
+          <input type="text" aria-label="First name" class="form-control" v-model="proveedoresID.ProvEmail">
+        </div>                       
+        <div class="input-group separacion">
+          <span class="my-auto" style="padding-right: 5px;">N.Empresa: </span>
+          <input type="text" aria-label="First name" class="form-control" v-model="proveedoresID.ProvNEmpresa">
+        </div>                                         
+        <div class="input-group separacion">
+          <span class="my-auto" style="padding-right: 5px;">Tipo Producto: </span>
+          <input type="text" aria-label="First name" class="form-control" v-model="proveedoresID.ProvTipProducto">
+        </div>                       
+        <div class="input-group separacion">
+          <span class="my-auto" style="padding-right: 5px;">Cuidad: </span>
+          <input type="text" aria-label="First name" class="form-control" v-model="proveedoresID.ProvCiudad">
+        </div>                       
+        <div class="input-group separacion">
+          <span class="my-auto" style="padding-right: 5px;">Dirección: </span>
+          <input type="text" aria-label="First name" class="form-control" v-model="proveedoresID.ProvDireccion">
+        </div>           
+        <div class="separacion row"><!-- Btns -->
+              <div class="col-md-6">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit" id="btn" disabled>Agregar</button>   
+              </div>
+              <div class="col-md-6">
+                <button class="btn btn-outline-warning my-2 my-sm-0" type="submit" id="btn" @click="ActualizarProv()">Actualizar</button> 
+              </div>
+        </div>                       
+        <div class="separacion">
+            <button class="btn btn-outline-danger my-2 my-sm-0" type="submit" id="btn" @click="EliminarProv(proveedoresID.ProvId)">Eliminar</button> 
+        </div>             
+    </div>  
+    <div class="col-md-4 my-auto" v-else> <!-- inputs default -->
         <div class="input-group">
           <span class="my-auto" style="padding-right: 5px;">Nit:</span>
           <input type="text" aria-label="First name" class="form-control" v-model="datos.ProvNit">
         </div>                       
         <div class="input-group separacion">
           <span class="my-auto" style="padding-right: 5px;">Teléfono:</span>
-          <input type="text" aria-label="First name" class="form-control" v-model="datos.ProvTelefono">
+          <input type="text" aria-label="First name" class="form-control" v-model="datos.ProvTelefono"  >
         </div>                       
         <div class="input-group separacion">
           <span class="my-auto" style="padding-right: 5px;">Email:</span>
-          <input type="text" aria-label="First name" class="form-control" v-model="datos.ProvEmail">
+          <input type="text" aria-label="First name" class="form-control" v-model="datos.ProvEmail"  >
         </div>                       
         <div class="input-group separacion">
           <span class="my-auto" style="padding-right: 5px;">N.Empresa: </span>
-          <input type="text" aria-label="First name" class="form-control" v-model="datos.ProvNEmpresa">
+          <input type="text" aria-label="First name" class="form-control" v-model="datos.ProvNEmpresa"  >
         </div>                                         
         <div class="input-group separacion">
           <span class="my-auto" style="padding-right: 5px;">Tipo Producto: </span>
-          <input type="text" aria-label="First name" class="form-control" v-model="datos.ProvTipProducto">
+          <input type="text" aria-label="First name" class="form-control" v-model="datos.ProvTipProducto"  >
         </div>                       
         <div class="input-group separacion">
           <span class="my-auto" style="padding-right: 5px;">Cuidad: </span>
-          <input type="text" aria-label="First name" class="form-control" v-model="datos.ProvCiudad">
+          <input type="text" aria-label="First name" class="form-control" v-model="datos.ProvCiudad"  >
         </div>                       
         <div class="input-group separacion">
           <span class="my-auto" style="padding-right: 5px;">Dirección: </span>
-          <input type="text" aria-label="First name" class="form-control" v-model="datos.ProvDireccion">
-        </div>                       
+          <input type="text" aria-label="First name" class="form-control" v-model="datos.ProvDireccion"  >
+        </div>           
         <div class="separacion row"><!-- Btns -->
               <div class="col-md-6">
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit" id="btn" @click.prevent="agregarProv()">Agregar</button> 
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit" id="btn" @click.prevent="agregarProv()">Agregar</button>   
               </div>
               <div class="col-md-6">
                 <button class="btn btn-outline-warning my-2 my-sm-0" type="submit" id="btn" >Actualizar</button> 
               </div>
         </div>                       
-        <div class="separacion">
-            <button class="btn btn-outline-danger my-2 my-sm-0" type="submit" id="btn" >Eliminar</button> 
-        </div>                       
-    </div>
+        <div class="separacion row"><!-- Btns -->
+              <div class="col-md-6">
+                <button class="btn btn-outline-danger my-2 my-sm-0" type="submit" id="btn" >Eliminar</button>   
+              </div>
+              <div class="col-md-6">
+                <a href="#/dashboard"><button class="btn btn-outline-info my-2 my-sm-0" type="submit" id="btn">Atrás</button></a>
+              </div>
+        </div>            
+    </div>    
   </div>
 </template>
 <style>
@@ -85,17 +135,19 @@
 #separacion{
   padding: 0;
   margin: 0;
-  
 }
+
 </style>
 
 <script>
 export default {
   data(){
     return{
-      ruta: 'http://localhost:4000/api/proveedor',
+      ruta: 'http://localhost:4000/api/proveedor/',
       datos: {ProvNit: '', ProvTelefono: '', ProvEmail: '', ProvNEmpresa: '', ProvTipProducto: '', ProvCiudad: '', ProvDireccion: ''},
-      proveedores: []
+      proveedores: [],
+      proveedoresID: {ProvNit: '', ProvTelefono: '', ProvEmail: '', ProvNEmpresa: '', ProvTipProducto: '', ProvCiudad: '', ProvDireccion: ''}, // actualizar y consulta por ID
+      editar: false
     };
   },
   created(){
@@ -114,8 +166,8 @@ export default {
       })
       .then( res => res.json())
       .then(data=> { 
-            console.log(data);
-            this.consultarProv()
+            this.consultarProv();
+            this.limpiar();
         })
         .catch(e =>{
             console.log(e);
@@ -133,12 +185,73 @@ export default {
       .then( res => res.json())
       .then(data=> {
             this.proveedores = data;
-            console.log(this.proveedores);
         })
         .catch(e =>{
             console.log(e);
         })
+    },
+    consultarProvID(codigo){
+      fetch(this.ruta + `/${codigo}`,{
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+          'Accept':'application/json',
+          'Content-type':'application/json'
+        },
+      })
+      .then( res => res.json())
+      .then( data=> {
+          this.proveedoresID = data[0];
+          this.editar = true
+        })
+        .catch(e =>{
+            console.log(e);
+        })
+    },
+    ActualizarProv(){
+      fetch(this.ruta,{
+        method: 'PUT',
+        mode: 'cors',
+        headers: {
+          'Accept':'application/json',
+          'Content-type': 'application/json'
+        },
+        body:JSON.stringify(this.proveedoresID)
+      })
+      .then( res => res.json())
+      .then( data=> {
+          this.consultarProv();
+          this.limpiar();
+          this.editar = false
+        })
+        .catch(e =>{
+            console.log(e);
+        })
+    },
+    EliminarProv(codigo){
+      fetch(this.ruta + `${codigo}`,{
+        method: 'DElETE',
+        mode: 'cors',
+        headers: {
+          'Accept':'application/json',
+          'Content-type': 'application/json'
+        },
+      })
+      .then( res => res.json())
+      .then( data=> {
+          this.consultarProv();
+          this.limpiar();
+          this.editar = false
+        })
+        .catch(e =>{
+            console.log(e);
+        })
+    },
+    limpiar(){
+      this.datos = {ProvNit: '', ProvTelefono: '', ProvEmail: '', ProvNEmpresa: '', ProvTipProducto: '', ProvCiudad: '', ProvDireccion: ''}
+      this.proveedoresID = {ProvNit: '', ProvTelefono: '', ProvEmail: '', ProvNEmpresa: '', ProvTipProducto: '', ProvCiudad: '', ProvDireccion: ''}
     }
+
   }
 }
 </script>
